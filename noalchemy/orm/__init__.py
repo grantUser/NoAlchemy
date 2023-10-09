4ef47bb98d2f1f15e48f94627ca5972521ea5b54
grant_user <140173422+grantUser@users.noreply.github.com>
2023-10-09 16:35:34 +0200

    [ADD] required key property

--- noalchemy/orm/__init__.py
+++ noalchemy/orm/__init__.py
@@ -1,23 +1,31 @@
 from bson import ObjectId
-
 from .. import Key
-
 
 class Base:
     def __init__(self, *args, **kwds):
-        class_annotations = self.__class__.__annotations__
-
-        valid_kwargs = {}
-        for key, value in kwds.items():
-            if annotation := class_annotations.get(key, False):
-                if isinstance(annotation, Key):
-                    annotation.type.content = value
-                    valid_kwargs[key] = annotation.type
-                else:
-                    valid_kwargs[key] = value
-
-        self.__dict__.update(valid_kwargs)
+        self._validate_required_keys(kwds)
+        self._initialize_keys(kwds)
         self._post_init()
+
+    def _validate_required_keys(self, kwds):
+        for key, type_instance in self.__class__.__annotations__.items():
+            if isinstance(type_instance, Key) and key not in kwds:
+                if type_instance.required:
+                    raise Exception(f"{key} is required.")
+
+    def _initialize_keys(self, kwds):
+        self.__dict__.update(self._process_keys(kwds))
+
+    def _process_keys(self, kwds):
+        processed_dict = {}
+        for key, type_instance in self.__class__.__annotations__.items():
+            value = kwds.get(key, None)
+            if isinstance(type_instance, Key):
+                type_instance.type.content = value
+                processed_dict[key] = type_instance.type
+            else:
+                processed_dict[key] = value
+        return processed_dict
 
     def _post_init(self):
         if not hasattr(self, "_id") or not ObjectId.is_valid(self._id):
@@ -25,14 +33,12 @@ class Base:
 
     def __setattr__(self, key, value):
         class_annotations = self.__class__.__annotations__
-
         if annotation := class_annotations.get(key, False):
             if isinstance(annotation, Key):
                 annotation.type.content = value
                 self.__dict__[key] = annotation.type
             else:
                 self.__dict__[key] = value
-
 
     def __call__(self, *args, **kwds):
         self.__init__(self, *args, **kwds)
