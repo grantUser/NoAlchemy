b04ae3ff13c2ad2a70f3afda3a3a81707bda328d
grant_user <140173422+grantUser@users.noreply.github.com>
2023-10-19 07:17:28 -0700

    [UPDATE] use bulk write operations & refacto

--- noalchemy/__init__.py
+++ noalchemy/__init__.py
@@ -3,6 +3,8 @@ from typing import Any
 from .engine import _engine
 from .types import Integer, Key, String
 
+from pymongo import InsertOne, UpdateOne, DeleteOne
+
 
 class create_engine:
     def __new__(self, *args: Any, **kwds: Any) -> None:
--- noalchemy/odm/session.py
+++ noalchemy/odm/session.py
@@ -6,17 +6,20 @@ from ..exc import *
 from ..types import Key
 from .models import models
 
+from .. import InsertOne, UpdateOne, DeleteOne
+
 
 class Transaction:
-    def __init__(self, bind=None, autocommit=False) -> None:
+    def __init__(self, bind=None) -> None:
         self.__bind = bind
-        self.__autocommit = autocommit
-        self.__to_insert = {}
-        self.__inserted = {}
-        self.__to_update = {}
-        self.__updated = {}
-        self.__to_delete = {}
-        self.__deleted = {}
+        self.bulk_operations = {}
+
+
+    def _get_bulk_operations(self, collection_name):
+        if collection_name not in self.bulk_operations:
+            self.bulk_operations[collection_name] = []
+
+        return self.bulk_operations[collection_name]
 
     def delete_one(self, object):
         if not hasattr(object, "__from__"):
@@ -26,11 +29,8 @@ class Transaction:
             raise Exception()
 
         collection = object.__collection_name__
-        if collection not in self.__to_delete:
-            self.__to_delete[collection] = []
-
-        if hasattr(object, "to_dict") and callable(object.to_dict):
-            self.__to_delete[collection].append(object.to_dict())
+        bulk_ops = self._get_bulk_operations(collection)
+        bulk_ops.append(DeleteOne({"_id": object._id}))
 
     def delete_many(self, objects):
         for object in objects:
@@ -41,169 +41,74 @@ class Transaction:
             raise Exception()
 
         collection = object.__collection_name__
-        if collection not in self.__to_insert:
-            self.__to_insert[collection] = []
-
-        for index, item in enumerate(self.__to_insert[collection]):
-            if item["_id"] == object._id:
-                self.__to_insert[collection][index] = object.to_dict()
-                return
-
-        if hasattr(object, "to_dict") and callable(object.to_dict):
-            self.__to_insert[collection].append(object.to_dict())
+        bulk_ops = self._get_bulk_operations(collection)
+        document = object.to_dict()
+        bulk_ops.append(InsertOne(document))
 
     def insert_many(self, objects):
         for object in objects:
             self.insert_one(object)
 
     def update_one(self, object):
-        if not hasattr(object, "__from__"):
-            raise Exception()
-
-        if object.__from__ != 1:
+        if not hasattr(object, "__from__") or object.__from__ != 1:
             raise Exception()
 
         if object.__eta__ != 2:
             return
 
         collection = object.__collection_name__
-        if collection not in self.__to_update:
-            self.__to_update[collection] = []
-
-        if hasattr(object, "items") and callable(object.items):
-            self.__to_update[collection].append(
-                {
-                    "object": object.to_dict(),
-                    "$set": {
-                        key: value
-                        for key, value in object.items()
-                        if key in object.__originals__.keys()
-                    },
-                }
-            )
+        bulk_ops = self._get_bulk_operations(collection)
+        document = object.to_dict()
+        update = {
+            "$set": {key: value for key, value in object.items() if key in object.__originals__.keys()}
+        }
+        bulk_ops.append(UpdateOne({"_id": document["_id"]}, update))
 
     def commit(self):
-        # order -> INSERT, UPDATE, DELETE
-        try:
-            for collection, objects in self.__to_insert.items():
-                collection = self.__bind.database[collection]
-
-                match len(objects):
-                    case n if n > 1:
-                        collection.insert_many(objects)
-                    case 1:
-                        collection.insert_one(objects[0])
-                    case 0:
-                        pass
-
-                if collection not in self.__inserted:
-                    self.__inserted[collection] = []
-
-                self.__inserted[collection].extend(objects)
-
-            self.__to_insert = {}
-            self.__inserted = {}
-
-            # update part
-            for collection, objects in self.__to_update.items():
-                collection = self.__bind.database[collection]
-
-                for object in objects:
-                    set = object.get("$set", {})
-                    object = object.get("object", None)
-
-                    if document_id := object.get("_id", None):
-                        collection.update_one({"_id": document_id}, {"$set": set})
-
-                if collection not in self.__updated:
-                    self.__updated[collection] = []
-
-                self.__updated[collection].extend(objects)
-
-            self.__to_update = {}
-            self.__updated = {}
-
-            # delete part
-
-            for collection, objects in self.__to_delete.items():
-                collection = self.__bind.database[collection]
-
-                for object in objects:
-                    if document_id_to_delete := object.get("_id", False):
-                        collection.delete_one({"_id": document_id_to_delete})
-
-                if collection not in self.__deleted:
-                    self.__deleted[collection] = []
-
-                self.__deleted[collection].extend(objects)
-
-            self.__to_delete = {}
-            self.__deleted = {}
-
-        except Exception as e:
-            self.rollback()
-            raise Exception(e)
+        for collection_name, bulk_ops in self.bulk_operations.items():
+            if bulk_ops:
+                try:
+                    collection = self.__bind.database[collection_name]
+                    collection.bulk_write(bulk_ops, ordered=True)
+                except Exception as e:
+                    self.rollback()
+                    raise e
 
     def rollback(self):
-        for collection, objects in self.__inserted.items():
-            collection = self.__bind.database[collection]
-
-            for object in objects:
-                collection.delete_one(object)
-
-        self.__to_insert = {}
-        self.__inserted = {}
-
-        for collection, objects in self.__updated.items():
-            collection = self.__bind.database[collection]
-
-            for object in objects:
-                set = object.get("$set", {})
-                object = object.get("object", None)
-
-                if document_id := object.get("_id", None):
-                    setset = {
-                        key: value for key, value in object.items() if key in set.keys()
-                    }
-
-                    collection.update_one({"_id": document_id}, {"$set": setset})
-
-        for collection, objects in self.__deleted.items():
-            collection = self.__bind.database[collection]
-
-            match len(objects):
-                case n if n > 1:
-                    collection.insert_many(objects)
-                case 1:
-                    collection.insert_one(objects[0])
-                case 0:
-                    pass
-
-        self.__to_delete = {}
-        self.__deleted = {}
+        for collection_name, bulk_ops in self.bulk_operations.items():
+            for operation in reversed(bulk_ops):
+                if operation.__class__ == DeleteOne:
+                    collection = self.__bind.database[collection_name]
+                    collection.insert_one(operation.filter)
+                elif operation.__class__ == InsertOne:
+                    collection = self.__bind.database[collection_name]
+                    collection.delete_one(operation.document)
+                elif operation.__class__ == UpdateOne:
+                    collection = self.__bind.database[collection_name]
+                    collection.update_one(operation.filter, {"$set": {key: None for key in operation.update["$set"]}})
+        self.bulk_operations = {}
 
     def __enter__(self):
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        if self.__autocommit:
-            self.commit()
+        pass
 
 
 class Session:
     def __init__(self, bind=None, autocommit=False) -> None:
         self.bind = bind
         self.autocommit = autocommit
-        self._transation = Transaction(bind=self.bind, autocommit=self.autocommit)
+        self._transaction = Transaction(bind=self.bind)
 
     def commit(self, *args, **kwds):
-        if self._transation:
-            with self._transation as transaction:
+        if self._transaction:
+            with self._transaction as transaction:
                 transaction.commit(*args, **kwds)
 
     def rollback(self, *args, **kwds):
-        if self._transation:
-            with self._transation as transaction:
+        if self._transaction:
+            with self._transaction as transaction:
                 transaction.rollback(*args, **kwds)
 
     def add(self, object):
@@ -220,7 +125,7 @@ class Session:
         if not hasattr(object, "_id") or not ObjectId.is_valid(object._id):
             raise Exception("_id incorrect", object)
 
-        if self._transation:
+        if self._transaction:
             with self._transation as transaction:
                 transaction.insert_one(object)
 
@@ -238,8 +143,8 @@ class Session:
         if not hasattr(object, "_id") or not ObjectId.is_valid(object._id):
             raise Exception("_id incorrect", object)
 
-        if self._transation:
-            with self._transation as transaction:
+        if self._transaction:
+            with self._transaction as transaction:
                 transaction.delete_one(object)
 
     def update(self, object):
@@ -256,8 +161,8 @@ class Session:
         if not hasattr(object, "_id") or not ObjectId.is_valid(object._id):
             raise Exception("_id incorrect", object)
 
-        if self._transation:
-            with self._transation as transaction:
+        if self._transaction:
+            with self._transaction as transaction:
                 transaction.update_one(object)
 
     def add_all(self, objects: list):
@@ -278,8 +183,8 @@ class Session:
             if not hasattr(object, "_id") or not ObjectId.is_valid(object._id):
                 raise Exception("_id incorrect", object)
 
-        if self._transation:
-            with self._transation as transaction:
+        if self._transaction:
+            with self._transaction as transaction:
                 transaction.insert_many(objects)
 
     def delete_all(self, objects: list):
@@ -300,13 +205,20 @@ class Session:
             if not hasattr(object, "_id") or not ObjectId.is_valid(object._id):
                 raise Exception("_id incorrect", object)
 
-        if self._transation:
-            with self._transation as transaction:
+        if self._transaction:
+            with self._transaction as transaction:
                 transaction.delete_many(objects)
 
     def query(self, *args, **kwds):
         kwds["bind"] = self.bind
         return self.Query(*args, **kwds)
+    
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.autocommit:
+            self.commit()
 
     class Query:
         def __init__(self, *args, **kwds) -> None:
@@ -427,55 +339,51 @@ class _scoped_session:
         self.session_factory = session_factory
         self._local = threading.local()
 
-    def __call__(self):
+    def _get_or_create_session(self):
         if not hasattr(self._local, "session"):
             self._local.session = self.session_factory()
         return self._local.session
+
+    def __call__(self, autocommit=False):
+        session = self._get_or_create_session()
+        session.autocommit = autocommit
+        return session
 
     def query(self, *args, collection=None, object=None):
-        if not hasattr(self._local, "session"):
-            self._local.session = self.session_factory()
-        return self._local.session.query(*args, collection=collection, object=object)
+        session = self._get_or_create_session()
+        return session.query(*args, collection=collection, object=object)
 
     def add(self, object):
-        if not hasattr(self._local, "session"):
-            self._local.session = self.session_factory()
-        return self._local.session.add(object)
+        session = self._get_or_create_session()
+        return session.add(object)
 
     def delete(self, object):
-        if not hasattr(self._local, "session"):
-            self._local.session = self.session_factory()
-        return self._local.session.delete(object)
+        session = self._get_or_create_session()
+        return session.delete(object)
 
     def add_all(self, objects):
-        if not hasattr(self._local, "session"):
-            self._local.session = self.session_factory()
-        return self._local.session.add_all(objects)
+        session = self._get_or_create_session()
+        return session.add_all(objects)
 
     def delete_all(self, objects):
-        if not hasattr(self._local, "session"):
-            self._local.session = self.session_factory()
-        return self._local.session.delete_all(objects)
+        session = self._get_or_create_session()
+        return session.delete_all(objects)
 
     def update(self, object):
-        if not hasattr(self._local, "session"):
-            self._local.session = self.session_factory()
-        return self._local.session.update(object)
+        session = self._get_or_create_session()
+        return session.update(object)
 
     def commit(self):
-        if not hasattr(self._local, "session"):
-            raise Exception("No session detected, unable to commit.")
-        return self._local.session.commit()
+        session = self._get_or_create_session()
+        return session.commit()
 
     def rollback(self):
-        if not hasattr(self._local, "session"):
-            raise Exception("No session detected, unable to rollback.")
-        return self._local.session.rollback()
+        session = self._get_or_create_session()
+        return session.rollback()
 
     def __enter__(self):
-        if not hasattr(self._local, "session"):
-            self._local.session = self.session_factory()
-        return self._local.session
+        session = self._get_or_create_session()
+        return session
 
     def __exit__(self, exc_type, exc_value, traceback):
         if hasattr(self._local, "session"):
