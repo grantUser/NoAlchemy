3c1ca50df8ede63e49b21d74451b90563ed904c3
grant_user <140173422+grantUser@users.noreply.github.com>
2023-10-20 02:17:57 -0700

    [UPDATE] setup and add version

--- noalchemy/__init__.py
+++ noalchemy/__init__.py
@@ -3,6 +3,7 @@ from typing import Any
 from .engine import _engine, InsertOne, UpdateOne, DeleteOne
 from .types import Integer, Key, String
 
+__version__ = "0.1.1"
 
 class create_engine:
     def __new__(self, *args: Any, **kwds: Any) -> None:
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="NoAlchemy",
-    version="0.1.0",
+    version="0.1.1",
     description="A MongoDB Object-Document Mapper (ODM) based on SQLAlchemy.",
     author="grantUser",
     url="https://github.com/grantUser/NoAlchemy",
