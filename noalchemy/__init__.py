08964390c0458dfbd5f8c1ca9ea9465826b01107
grant_user <140173422+grantUser@users.noreply.github.com>
2023-10-23 13:15:21 +0200

    [ADD] Dict & List accessibility

--- noalchemy/__init__.py
+++ noalchemy/__init__.py
@@ -1,7 +1,7 @@
 from typing import Any
 
 from .engine import _engine, InsertOne, UpdateOne, DeleteOne
-from .types import Integer, Key, String
+from .types import Integer, Key, String, Dict, List
 from .version import __version__, version
 
 class create_engine:
