98227c5b6b0807c2447dcaaf10b4e8d71fd50204
grant_user <140173422+grantUser@users.noreply.github.com>
2023-10-23 17:59:31 +0200

    [FIX] support for native types

--- noalchemy/odm/document.py
+++ noalchemy/odm/document.py
@@ -33,7 +33,8 @@ class Document:
                 type.__collection_name__ = cls.__collection_name__
                 type.__key__ = key
                 type.__object__ = cls
-                setattr(cls, key, type)
+
+            setattr(cls, key, type)
 
         with cls.Session as session:
             if (
@@ -73,6 +74,11 @@ class Document:
 
                 new_instance.content = kwds.get(key, None)
                 self.__dict__[key] = new_instance
+            else:
+                if value := kwds.get(key, False):
+                    if not isinstance(value, instance):
+                        raise Exception("Incorrect type, " + type(value))
+                    self.__dict__[key] = value
 
         if _id := kwds.get("_id", False):
             if ObjectId.is_valid(_id):
@@ -85,7 +91,8 @@ class Document:
             self.__dict__["__eta__"] = 2
 
         instances = self.__class__.__annotations__
-        if instance := instances.get(key, False) or key in whitelist_key:
+        if instance := instances.get(key, False):
+
             if isinstance(instance, Key):
                 if self.__eta__ == 2 and (
                     hasattr(self, "__from__") and self.__from__ == 1
@@ -93,19 +100,24 @@ class Document:
                     if key not in self.__originals__:
                         original = self.__dict__.get(key, None)
                         self.__originals__[key] = original.content
+                        self.Session._update(self)
 
                 instance.type.content = value
                 self.__dict__[key] = instance.type
-                self.Session._update(self)
-
-            elif key in whitelist_key:
+            else:
                 self.__dict__[key] = value
 
-    def to_dict(self):
+        elif key in whitelist_key:
+            self.__dict__[key] = value
+
+    def to_dict(self, exclude: list = None):
+        if not exclude:
+            exclude = []
+
         readable_dict = {
             key: value
             for key, value in self.__dict__.items()
-            if key not in whitelist_key
+            if key not in whitelist_key and key not in exclude
         }
 
         for _dict_key, _dict_class in readable_dict.items():
@@ -117,11 +129,14 @@ class Document:
 
         return readable_dict
 
-    def items(self):
+    def items(self, exclude: list = None):
+        if not exclude:
+            exclude = []
+
         readable_dict = {
             key: value
             for key, value in self.__dict__.items()
-            if key not in whitelist_key
+            if key not in whitelist_key and key not in exclude
         }
 
         for _dict_key, _dict_class in readable_dict.items():
