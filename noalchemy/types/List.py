cecf9b391a00c082b787591195d99dee92816c01
grant_user <140173422+grantUser@users.noreply.github.com>
2023-10-23 17:58:02 +0200

    [ADD] new List func

--- noalchemy/types/List.py
+++ noalchemy/types/List.py
@@ -1,12 +1,29 @@
 class List:
-    def __init__(self) -> None:
+    def __init__(self, native_type=None) -> None:
         self.__noalchemy_type__ = True
         self.content = []
+    
+        if not isinstance(native_type, (list, tuple)):
+            native_type = [native_type]
+
+        self.native_types = native_type
+
+    def _check_native_type(self, data):
+        if self.native_types is not None and not any(isinstance(data, t) for t in self.native_types):
+            raise TypeError(f"Element must be one of the types {self.native_types}")
+        
+    @property
+    def value(self):
+        return self.content
 
     def append(self, data):
+        self._check_native_type(data)
         self.content.append(data)
 
     def extend(self, data):
+        if self.native_types is not None:
+            if not all(any(isinstance(item, t) for t in self.native_types) for item in data):
+                raise TypeError(f"All elements must be one of the types {self.native_types}")
         self.content.extend(data)
 
     def __len__(self):
@@ -23,6 +40,7 @@ class List:
 
     def __setitem__(self, index, value):
         if 0 <= index < len(self.content):
+            self._check_native_type(value)
             self.content[index] = value
         else:
             raise IndexError("Index out of range")
